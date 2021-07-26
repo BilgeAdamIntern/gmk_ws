63f4114a85ae8a0c1423eeafcd109a69e4fab323
BilgeAdamIntern <bilgeadamintern@gmail.com>
2021-07-26 16:56:51 +0300

    little patchs

--- frontend/src/components/TableRows.js
+++ frontend/src/components/TableRows.js
@@ -7,17 +7,34 @@ class TableRows extends Component {
 
         return (
             <div>
+                <thead>
+                <tr>
+                    <th>Ad</th>
+                    <th>Soyad</th>
+                    <th>TelefonNo</th>
+                    <th>TCKNO</th>
+                    <th>Departman</th>
+                    <th>Görev</th>
+                    <th>Yaş</th>
+                </tr>
                 {!!this.props.list && !!this.props.list.length && this.props.list.map(each => {
-                    return (<div>
-                        <th>{each.firstName}</th>
-                        <th>{each.secondName}</th>
-                        <th>{each.phoneNumber}</th>
-                        <th>{}</th>
-                        <th>{each.department}</th>
-                        <th>{each.duty}</th>
-                        <th>{each.age}</th>
-                    </div>);
+                    return (
+
+
+                        <tr>
+                            <th>{each.firstName}</th>
+                            <th>{each.secondName}</th>
+                            <th>{each.phoneNumber}</th>
+                            <th>{each.tcno}</th>
+                            <th>{each.department}</th>
+                            <th>{each.duty}</th>
+                            <th>{each.age}</th>
+                        </tr>
+
+
+                    );
                 })}
+                </thead>
             </div>
         );
     }
