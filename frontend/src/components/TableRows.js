e9679fe5cc0730ad55ccc11a3322655ce4541c99
BilgeAdamIntern <bilgeadamintern@gmail.com>
2021-07-27 10:10:21 +0300

    little patchs

--- frontend/src/components/TableRows.js
+++ frontend/src/components/TableRows.js
@@ -1,32 +1,32 @@
 import React, {Component} from "react";
+import  '../ForRows.css';
 
 class TableRows extends Component {
 
 
     render() {
-
         return (
             <div>
                 <thead>
                 <tr>
-                    <th>Ad</th>
-                    <th>Soyad</th>
-                    <th>TelefonNo</th>
-                    <th>TCKNO</th>
-                    <th>Departman</th>
-                    <th>Görev</th>
-                    <th>Yaş</th>
+                    <th className="long-rows">Ad</th>
+                    <th className="long-rows">Soyad</th>
+                    <th className="long-rows">TelefonNo</th>
+                    <th className="long-rows">TCKNO</th>
+                    <th className="long-rows">Departman</th>
+                    <th className="long-rows">Görev</th>
+                    <th className="short-rows">Yaş</th>
                 </tr>
                 {!!this.props.list && !!this.props.list.length && this.props.list.map(each => {
                     return (
                         <tr>
-                            <th>{each.firstName}</th>
-                            <th>{each.secondName}</th>
-                            <th>{each.phoneNumber}</th>
-                            <th>{each.tcno}</th>
-                            <th>{each.department}</th>
-                            <th>{each.duty}</th>
-                            <th>{each.age}</th>
+                            <th className="long-rows">{each.firstName}</th>
+                            <th className="long-rows">{each.secondName}</th>
+                            <th className="long-rows">{each.phoneNumber}</th>
+                            <th className="long-rows">{each.tcno}</th>
+                            <th className="long-rows">{each.department}</th>
+                            <th className="long-rows">{each.duty}</th>
+                            <th className="short-rows">{each.age}</th>
                         </tr>
                     );
                 })}
